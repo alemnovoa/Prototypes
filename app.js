f697a4e20056a6c6d24ed5ce250303c569203faf
Alejandro Martin Novoa <alejandromnovoa@gmail.com>
2020-04-10 13:30:36 -0300

    Version1

---
+++ app.js
@@ -0,0 +1,39 @@
+function Cliente(nombre, saldo){
+     this.nombre = nombre;
+     this.saldo = saldo;
+}
+
+// crear prototype
+Cliente.prototype.tipoCliente = function(){
+     let tipo;
+     if(this.saldo > 1000){
+          tipo = 'Gold';
+     } else if(this.saldo > 500) {
+          tipo = 'Platinum';
+     } else {
+          tipo = 'Normal';
+     }
+     return tipo;
+}
+
+// Prototype que imprime saldo y nombre
+Cliente.prototype.nombreClienteSaldo = function() {
+     return `Nombre: ${this.nombre}, Tu saldo es de ${this.saldo},
+     Tipo Cliente ${this.tipoCliente() }`;
+}
+
+// Retirar saldo
+Cliente.prototype.retirarSaldo = function(retiro) {
+     return this.saldo -= retiro;
+}
+
+const cliente1 = new Cliente ('Pedro', 100);
+const cliente2 = new Cliente ('Matilda', 600);
+const cliente3 = new Cliente ('Juan', 1200);
+
+//ejemplo de retiro de dinero y cambio de categoria
+cliente2.retirarSaldo(300);
+
+console.log(cliente1.nombreClienteSaldo());
+console.log(cliente2.nombreClienteSaldo());
+console.log(cliente3.nombreClienteSaldo());
